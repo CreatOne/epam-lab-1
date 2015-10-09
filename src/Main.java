b86cfceea0cc458cac366a232dda0c61828f73dc
Andy Govorov <thegovorovs@gmail.com>
2015-10-09 13:43:32 +0600

    added javadocs comments

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
-
+/**
+ * Taks number 9 - Planes
+ * @author Andrey Govorov
+ * @date 09.10.2015
+ * @version 1
+ */
 public class Main {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello");
